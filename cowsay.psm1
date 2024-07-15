fa73a74345402f2372a4ede634d0d7d17012a9f4
Anubhav Acharya <aacharya@epic.com>
2024-07-15 16:42:01 -0500

    Text Display Changes
    Moved away from the hard limit of only
    80 characters per line.
    Made it so that the message given isn't
    changed.
    Extra sanitizations added.

--- cowsay.psm1
+++ cowsay.psm1
@@ -1,67 +1,65 @@
 #
-#  Posh-Cowsay
-#  PowerShell version of the venerable cowsay unix program.
+#	Posh-Cowsay
+#	PowerShell version of the venerable cowsay unix program.
 #
-#  Copyright (c) 2014 John Kane
-#  https://github.com/kanej/posh-cowsay
-#  
-#  Based on Tony Monroe's cowsay: 
-#  http://www.nog.net/~tony/warez/cowsay-3.03.tar.gz
+#	Copyright (c) 2014 John Kane
+#	https://github.com/kanej/posh-cowsay
+#	
+#	Based on Tony Monroe's cowsay: 
+#	http://www.nog.net/~tony/warez/cowsay-3.03.tar.gz
 #
-#  Licensed under the GNU GPL version 3.0
+#	Licensed under the GNU GPL version 3.0
 #
 
 #requires -Version 2.0
 
 # Posh-Cowsay Version
-$version = "0.2.0"
-
-# Max Width of the Speech Bubble
-$bubbleWidth = 40
+$version = "0.2.1"
 
 # The different modes that are supported
 $modes = @{
-  "-b"= @("==", ' ') # Borg
-  "-d"= @("XX", 'U') # Dead
-  "-g"= @('$$', ' ') # Greedy
-  "-p"= @("@@", ' ') # Paranoid
-  "-s"= @("**", 'U') # Stoned
-  "-y"= @("..", ' ') # Youthful
+	"-b"= @("==", ' ') # Borg
+	"-d"= @("XX", 'U') # Dead
+	"-g"= @('$$', ' ') # Greedy
+	"-p"= @("@@", ' ') # Paranoid
+	"-s"= @("**", 'U') # Stoned
+	"-y"= @("..", ' ') # Youthful
 }
 
 # Public
 
 <# 
 .Synopsis
-  Prints the given text to the console as if a cow had said it.
+	Prints the given text to the console as if a cow had said it.
 .Description
-  Posh-Cowsay generates an ASCII art picture of a cow saying something
-  provided by the user. It word-wraps the message at about 40
-  columns, and prints the cow saying the given message on standard
-  output.
+	Posh-Cowsay generates an ASCII art picture of a cow saying something
+	provided by the user. It word-wraps the message at about 40
+	columns, and prints the cow saying the given message on standard
+	output.
 
-  Different modes can be enabled by passing the appropriate option.
-  For instance -d will enable Dead mode, were the cow shown appears
-  to be dead. The complete list of options are:
+	Different modes can be enabled by passing the appropriate option.
+	For instance -d will enable Dead mode, were the cow shown appears
+	to be dead. The complete list of options are:
 
-    Borg     -b
-    Dead     -d
-    Greedy   -g
-    Paranoid -p
-    Stoned   -s
-    Youthful -y
+		Borg		 -b
+		Dead		 -d
+		Greedy		 -g
+		Paranoid	 -p
+		Stoned		 -s
+		Youthful	 -y
 
-  The -version or -v option will display the version of Posh-Cowsay.
+	The --version or -v option will display the version of Posh-Cowsay.
 .Link
-  https://github.com/kanej/posh-cowsay
+	https://github.com/kanej/posh-cowsay
 
 .Example
-  cowsay moo
+	cowsay moo
 
-  Description
-  -----------
-  Takes "moo" to be the message and prints the text within a
-  speech bubble, followed by the cow:
+	Description
+	-----------
+	Takes "moo" to be the message and prints the text within a
+	speech bubble, followed by the cow:
+
 
    _____
   < moo >
@@ -73,11 +71,11 @@ $modes = @{
                 ||     ||
 
 .Example
-  "moo" | cowsay
+	"moo" | cowsay
 
-  Description
-  -----------
-  The message can be piped in as well, giving the same result:
+	Description
+	-----------
+	The message can be piped in as well, giving the same result:
 
    _____
   < moo >
@@ -89,9 +87,9 @@ $modes = @{
                 ||     ||
   
 .Example
-  cowsay -b Resistance is Bovine
+	cowsay -b Resistance is Bovine
 
-  Description
+	Description
   -----------
   The borg mode can be enabled by passing -b option, giving a cow that
   has joined the Collective:
@@ -106,179 +104,161 @@ $modes = @{
                 ||     ||
 #>
 function Cowsay() {
-  $params
+	$params
 
-  $messageArgs = @()
-  $eyes = "oo"
-  $tongue = " "
+	$messageArgs = @()
+	$eyes = "oo"
+	$tongue = " "
 
-  foreach($arg in $args) {
+	foreach($arg in $args) {
 
-    if($arg -eq "-v" -or $arg -eq "-version") {
-      Print-Version
-      return
-    }
+		if($arg -eq "-v" -or $arg -eq "--version") {
+			Print-Version
+			return
+		}
 
-    if($modes.keys -contains $arg) {
-      $eyes   = $modes[$arg][0]
-      $tongue = $modes[$arg][1]
-      continue
-    }
+		if($modes.keys -contains $arg) {
+			$eyes	 = $modes[$arg][0]
+			$tongue = $modes[$arg][1]
+			continue
+		}
 
-    $messageArgs += $arg
-  }
+		$messageArgs += $arg
+	}
 
-  $inputList = @($input)
-  if ($inputList.Count -eq 0) {
-    $params = ,$messageArgs
-  } else {
-    $params = ,$messageArgs +@($inputList)
-  }
+	$inputList = @($input)
+	if ($inputList.Count -eq 0) {
+		$params = ,$messageArgs
+	} else {
+		$params = ,$messageArgs +@($inputList)
+	}
 
-  $message = [string[]]$params
+	$message = [string[]]$params
 
-  Print-MessageBubble($message) 
+	Print-MessageBubble($message) 
 
-  Print-Cow $eyes $tongue
+	Print-Cow $eyes $tongue
 }
 
 # Private
 
 function Print-MessageBubble($message) {
-  $lines = Convert-MessageToLines($message)
-  $lineWidth = Max-Width($lines)
+	$lines = Convert-MessageToLines($message)
+	$lineWidth = Max-Width($lines)
 
-  Write-MessageBubbleBoundaryLine -lineWidth $lineWidth -boundaryChar '_' 
+	Write-MessageBubbleBoundaryLine -lineWidth $lineWidth -boundaryChar '_' 
 
-  foreach ($index in 0..($lines.length - 1)) {
-    $delimiters = Determine-MessageBubbleDelimiters $index $lines.length
-    $paddedLine = ' ' + $lines[$index] + ' '
-    Write-MessageBubbleLine -lineWidth $lineWidth -delimiters $delimiters -text $paddedLine
-  }
+	foreach ($index in 0..($lines.length - 1)) {
+		$delimiters = Determine-MessageBubbleDelimiters $index $lines.length
+		$paddedLine = ' ' + $lines[$index] + ' '
+		Write-MessageBubbleLine -lineWidth $lineWidth -delimiters $delimiters -text $paddedLine
+	}
 
-  Write-MessageBubbleBoundaryLine -lineWidth $lineWidth -boundaryChar '-'
+	Write-MessageBubbleBoundaryLine -lineWidth $lineWidth -boundaryChar '-'
 }
 
 function Print-Cow($eyes="oo", $tongue=" ") {
-  Write-Output "      \  ^__^             "
-  Write-Output "       \ ($eyes)\________    "
-  Write-Output "         (__)\        )\/\"
-  Write-Output "          $tongue   ||----w |   "
-  Write-Output "              ||     ||   "
+	Write-Output "      \  ^__^             "
+	Write-Output "       \ ($eyes)\________    "
+	Write-Output "         (__)\        )\/\"
+	Write-Output "          $tongue   ||----w |   "
+	Write-Output "              ||     ||   "
 }
 
 function Print-Version() {
- Print-MessageBubble("Posh-Cowsay version $version") 
- Print-Cow
+	Print-MessageBubble("Posh-Cowsay version $version") 
+	Print-Cow
 }
 
 # Helper Functions
 
 function Convert-MessageToLines($message) {
-  $words = Split-Message $message  
-  $lines = @()
-  $line = ""
+	$words = Split-Message $message	
+	$lines = @()
+	$line = ""
 
-  foreach($word in $words) {
-    if(($line.length + $word.length + 1) -gt $bubbleWidth) {
-      if($line -ne "") {
-        $lines += ,$line
-      }
-      $line = $word 
-    } else {
-      if($line -eq "") {
-        $line = $word
-      } else {
-        $line += " " + $word
-      }
-    }
-  }
+	foreach($word in $words) {
+		if(($line.length + $word.length + 1) -gt $bubbleWidth) {
+			if($line -ne "") {
+				$lines += ,$line
+			}
+			$line = $word 
+		} else {
+			if($line -eq "") {
+				$line = $word
+			} else {
+				$line += " " + $word
+			}
+		}
+	}
 
-  $lastLine = $line
+	$lastLine = $line
 
-  $lines += ,$lastLine
-  return ,$lines 
+	$lines += ,$lastLine
+	return ,$lines 
 }
 
 function Split-Message($message) {
-  $wordsSplitOnSpaces = $message.split(" ")
-  
-  $words = [string[]]@()
+	$rawLines = $message.split("`n")
 
-  foreach($longWord in $wordsSplitOnSpaces) {
-    $splitWords = Split-Word($longWord)
-    foreach($word in $splitWords) {
-      if($word -ne "") {
-        $words+= ,[string]$word
-      }
-    }
-  }
+	$lines =[string[]]@()
 
-  return ,[string[]]$words
-}
+	foreach ($line in $rawLines) {
+		$fixedLine = [string]::join("    ",($line.Split("	")))
+		$lines += ,[string]$fixedLine.Trim("`r").trimEnd()
+	}
 
-function Split-Word($word) {
-  if($word.length -le $bubbleWidth) {
-    return ,[string[]]@($word)
-  }
+	# If the last line is empty, remove it
+	if ([string]::IsNullOrWhiteSpace($lines[-1])) {
+		$lines = $lines[0..($lines.Length - 2)]
+	}
 
-  $splits = [string[]]@()
-
-  foreach($i in (0..($word.length / $bubbleWidth))) {
-    $startPoint = ($i * $bubbleWidth)
-    if(($startPoint + $bubbleWidth) -gt $word.length) {
-      $splits += $word.substring($startPoint)
-    } else {
-      $splits += $word.substring($startPoint, $bubbleWidth)
-    }
-  }
-
-  return ,[string[]]$splits
+	return ,[string[]]$lines
 }
 
 function Max-Width($lines) {
-  $maxLength = 0
-  foreach($line in $lines) {
-    if($line.length -gt $maxLength) {
-      $maxLength = $line.length
-    }
-  }
-  
-  return $maxLength
+	$maxLength = 0
+	foreach($line in $lines) {
+		if($line.length -gt $maxLength) {
+			$maxLength = $line.length
+		}
+	}
+	
+	return $maxLength
 }
 
 function Write-MessageBubbleBoundaryLine($lineWidth, $boundaryChar) {
-  Write-MessageBubbleLine -lineWidth $lineWidth `
-                          -delimiters '  ' `
-                          -text ("".padRight($lineWidth + 2, $boundaryChar))
+	Write-MessageBubbleLine -lineWidth $lineWidth `
+							-delimiters '  ' `
+							-text ("".padRight($lineWidth + 2, $boundaryChar))
 }
 
 function Write-MessageBubbleLine($lineWidth, $delimiters, $text) {
-    $line = $delimiters[0] + ($text.padRight($lineWidth + 2, ' ')) + $delimiters[1]
-    Write-Output $line.trimEnd()
+	$line = $delimiters[0] + ($text.trimEnd().padRight($lineWidth + 2, ' ')) + $delimiters[1]
+	Write-Output $line.trimEnd()
 }
 
 function Determine-MessageBubbleDelimiters($lineNumber, $totalNumberOfLines) {
-  # single line
-  if($totalNumberOfLines -eq 1) {
-    return '<>'
-  }
+	# single line
+	if($totalNumberOfLines -eq 1) {
+		return '<>'
+	}
 
-  # first line
-  if($lineNumber -eq 0) {
-    return '/\'
-  }
+	# first line
+	if($lineNumber -eq 0) {
+		return '/\'
+	}
 
-  # last line
-  if($lineNumber -eq ($totalNumberOfLines -1)) {
-    return '\/'
-  }
+	# last line
+	if($lineNumber -eq ($totalNumberOfLines -1)) {
+		return '\/'
+	}
 
-  # middle line
-  return '||'
+	# middle line
+	return '||'
 }
 
 # Exports
 Export-ModuleMember Cowsay
-Export-ModuleMember Split-Word
 Export-ModuleMember Split-Message
+
